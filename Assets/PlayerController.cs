3b777d693a58c172882677ab9a07c6d91dda8a53
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2024-02-12 12:33:51 -0400

    Added blendtree

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,22 +5,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 movement;
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float lastInputX, lastInputY;
 
     public void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     public void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
     }
 
     public void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        HandleMove();
+        HandleAnimation();
+    }
+
+    private void HandleMove()
+    {
+        _rb.MovePosition(_rb.position + _movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void HandleAnimation()
+    {
+
+        if (_movement != Vector2.zero)
+        {
+            _animator.SetFloat("MoveInputX", _movement.x);
+            _animator.SetFloat("MoveInputY", _movement.y);
+            _animator.SetBool("Moving", true);
+
+            if (_movement == new Vector2(0, -1))
+                _animator.SetFloat("MoveDir", 0);
+            if (_movement == new Vector2(0, 1))
+                _animator.SetFloat("MoveDir", 1);
+            if (_movement == new Vector2(1, 0))
+                _animator.SetFloat("MoveDir", 2);
+            if (_movement == new Vector2(-1, 0))
+                _animator.SetFloat("MoveDir", 3);
+            
+            
+        }
+        else
+        {
+            _animator.SetBool("Moving", false);
+
+        }
+       
     }
 }
